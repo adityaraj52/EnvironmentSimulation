3027e7378e8c00ab4417c0c2cb831050a31744b1
Aditya Raj <aditya.raj@tu-clausthal.de>
2016-07-27 02:00:17 +0200

    Modified pom to add default goal clean and added test to unmarshal osm file and further read the contents

--- src/test/java/environment/dataimport/TestCOSMReader.java
+++ src/test/java/environment/dataimport/TestCOSMReader.java
@@ -7,9 +7,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openstreetmap.osmosis.tagfilter.v0_6.TagFilter;
 
-//import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
+import java.io.File;
 import java.net.MalformedURLException;
 import java.util.HashSet;
 import java.util.Map;
@@ -47,14 +48,12 @@ public final class TestCOSMReader
     @Test
     public void testOSMRead() throws IOException, JAXBException
     {
+        System.out.println( "\nTest to create a sample xml file in target/map.osm location based on OSM Schema" );
         Assume.assumeNotNull( m_source );
 
         final Osm l_version = new Osm();
-        //l_version.setVersion( 123 );
         l_version.setCopyright( "hello" );
-        //JAXBContext.newInstance( Osm.class ).createMarshaller().marshal( l_version, new File( "src/main/instance1.xml" ) );
-
-
+        JAXBContext.newInstance( Osm.class ).createMarshaller().marshal( l_version, new File( "target/map.osm" ) );
         final IReader<Osm> l_osm = new CXMLReader<>( m_source );
         System.out.println( l_osm.get().getVersion() );
         System.out.println( l_osm.get().getBounds() );
@@ -70,6 +69,7 @@ public final class TestCOSMReader
     @Test
     public void testTagFilter() throws IOException, JAXBException
     {
+        System.out.println( "\nTest to check tag filter (Still has to tested)\n" );
         Assume.assumeNotNull( m_source );
 
         final IReader<Osm> l_osm = new CXMLReader<>( m_source );
@@ -88,5 +88,22 @@ public final class TestCOSMReader
                         )
         );
     }
+
+    /**
+     * test for OSM file unmarshalling and reading the contents from the object tree
+     *
+     * @throws IOException on io reading
+     * @throws JAXBException on XML parsing error
+     */
+    @Test
+    public void unmarshall() throws IOException, JAXBException
+    {
+        System.out.println( "\nTest to read a sample xml file stored in xsd/map.osm location and further store the elements in OSM class" );
+        final Osm l_read = (Osm)JAXBContext.newInstance( Osm.class ).createUnmarshaller( ).unmarshal( new File( "src/main/xsd/map.osm" ) );
+        System.out.println( l_read.getVersion() );
+        System.out.println( l_read.copyright + "\n" );
+
+    }
+
 
 }
